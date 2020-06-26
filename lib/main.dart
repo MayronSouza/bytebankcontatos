f388da1b5a585dfd24c215ffd374e8f3f7d059a2
Mayron Souza <mayrondrummer@gmail.com>
2020-06-26 14:23:38 -0300

    Implementando o Dashboard

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bytebankcontatos/screens/dashboard.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(BytebankContactsApp());
@@ -6,30 +7,15 @@ class BytebankContactsApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Dashboard'),
-          // centerTitle: true,
-        ),
-        body: Column(
-          children: <Widget>[
-            Image.asset('assets/images/bytebank_logo.png'),
-            Container(
-              color: Colors.green,
-              height: 120.0,
-              width: 100.0,
-              child: Column(
-                children: <Widget>[
-                  Icon(
-                    Icons.people,
-                  ),
-                  Text('Contacts')
-                ],
-              ),
-            ),
-          ],
+      theme: ThemeData(
+        primaryColor: Colors.green[900],
+        accentColor: Colors.blueAccent[700],
+        buttonTheme: ButtonThemeData(
+          buttonColor: Colors.blueAccent[700],
+          textTheme: ButtonTextTheme.primary,
         ),
       ),
+      home: Dashboard(),
     );
   }
 }
---
+++ lib/screens/dashboard.dart
@@ -0,0 +1,50 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Dashboard'),
+        // centerTitle: true,
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Image.asset('assets/images/bytebank_logo.png'),
+          ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Container(
+              padding: EdgeInsets.all(8.0),
+              color: Theme.of(context).primaryColor,
+              height: 100.0,
+              width: 180.0,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: <Widget>[
+                  Icon(
+                    Icons.people,
+                    color: Colors.white,
+                    size: 24.0,
+                  ),
+                  Text(
+                    'Contacts',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 16.0,
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
