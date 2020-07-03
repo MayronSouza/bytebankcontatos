1dbce86c48ca01d02bb0e1132d57bfa6050dca6e
MayronGMZ <mayron.souza@gmz.solutions>
2020-07-03 16:14:10 -0300

    Utilizando o interceptador do http e configurando  o logging interceptor

--- lib/http/web_cliente.dart
+++ lib/http/web_cliente.dart
@@ -4,19 +4,29 @@ import 'package:http_interceptor/http_interceptor.dart';
 class LoggingInterceptor implements InterceptorContract {
   @override
   Future<RequestData> interceptRequest({RequestData data}) async {
-    print(data);
+    print('Reques');
+    print('url: ${data.url}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
     return data;
   }
 
   @override
   Future<ResponseData> interceptResponse({ResponseData data}) async {
-    print(data);
+    print('Reques');
+    print('headers: ${data.statusCode}');
+    print('headers: ${data.headers}');
+    print('body: ${data.body}');
     return data;
   }
 }
 
 void findAll() async {
+  final http.Client client = HttpClientWithInterceptor.build(
+    interceptors: [LoggingInterceptor()],
+  );
+
   final http.Response res =
-      await http.get('http://192.168.122.1:8080/transactions');
+      await client.get('http://192.168.122.1:8080/transactions');
   print(res.body);
 }
